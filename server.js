de86ed7d47a5d284db4c75a29e67ef12a53fdd2f
jronnomo <ggronnii@gmail.com>
2023-03-12 12:01:08 -0400

    more ToDoExample learning

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const mongoose = require('mongoose')
 
 require('dotenv').config({path: './config/.env'})
 
@@ -21,7 +21,12 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(logger('dev'))
+
+//Logging
+if (process.env.NODE_ENV === 'development'){
+  app.use(logger('dev'))
+}
+
 // Sessions
 app.use(
     session({
@@ -40,7 +45,6 @@ app.use(flash())
   
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+
+const PORT = process.env.PORT || 2121
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
